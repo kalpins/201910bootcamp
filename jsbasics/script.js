aca3616c497cd6ebdc58531dabf80ffc979bc9bf
Rolands <rolands.kalpins@gmail.com>
2019-10-30 17:41:06 +0200

    js basics

---
+++ jsbasics/script.js
@@ -0,0 +1,46 @@
+console.log("sveika pasaule")
+
+var nosaukums = "mans nosaukums";
+console.log(nosaukums); //mans nosaukums
+
+console.log(typeof (nosaukums)); //string
+
+nosaukums = 8;
+console.log(nosaukums); //8
+console.log(typeof (nosaukums)) //number
+
+// console.log(Nosaukums); //ERROR
+
+// const skolasNosaukums = "Riga coding school";
+// const skolas_nosaukums = "Riga coding school";
+
+// skolasNosaukums = "Rīgas programmēšanas skola"; //ERROR
+// console.log(skolasNosaukums);
+
+var a;
+console.log(a); //undefined
+a = 7;
+var b = a;
+// alert(b);
+
+q = 1;
+var w = 2;
+var e = "I'm string"
+
+// w = q
+console.log(w)
+
+q = q + w; //3
+w = w + w; //4
+q = q + 1; //4
+
+var z = "6";
+console.log(z + 1); //61
+console.log(z - 1); //5
+
+console.log(33 * 2); //66
+console.log(44 / 2); //22
+console.log(33 / 2); //16.5
+console.log(1 / 3)
+
+console.log(33 % 4) //1
